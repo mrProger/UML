49f10dff6ab8adb0e19696c02834e821ae1b0df7
mrProger <60040129+mrProger@users.noreply.github.com>
2021-04-14 15:26:17 +0600

    Add files via upload

---
+++ Client-Server/Client/Client/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Socket handler;
+        static int port = 8005;
+        static string address = "127.0.0.1";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Run();
+        }
+
+        private void Run() 
+        {
+            Logs.Document.Blocks.Clear();
+
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+ 
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // подключаемся к удаленному хосту
+                socket.Connect(ipPoint);
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+ 
+                // получаем ответ
+                data = new byte[256]; // буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // количество полученных байт
+ 
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                Console.WriteLine("ответ сервера: " + builder.ToString());
+ 
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+        }
+
+        private void AddMessageInLogs(string Message) { Logs.Document.Blocks.Add(new Paragraph(new Run(Message))); }
+
+        private void DisconnectFromServerButtonClick(object sender, RoutedEventArgs e) 
+        {
+            if (handler != null)
+            {
+                AddMessageInLogs("Вы были отключены от сервера");
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+        }
+    }
+}
