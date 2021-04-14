d8f11f199d0feef4784f4ea79503d901385744f8
mrProger <60040129+mrProger@users.noreply.github.com>
2021-04-14 15:29:05 +0600

    Add files via upload

---
+++ C#/Хребтов_Упражнение4/Хребтов_Упражнение4/ChildForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Хребтов_Упражнение4
+{
+    public partial class ChildForm : ParentForm
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+        private void frmatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toggleForegroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ToggleMenuItem.Checked)
+            {
+                ToggleMenuItem.Checked = false;
+                ChildTextBox.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                ToggleMenuItem.Checked = true;
+                ChildTextBox.ForeColor = System.Drawing.Color.Blue;
+            }
+        }
+    }
+}
