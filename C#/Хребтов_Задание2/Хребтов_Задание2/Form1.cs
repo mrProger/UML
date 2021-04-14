9c5e815aee55b8b30e76a8252d8be5be66a3e998
mrProger <60040129+mrProger@users.noreply.github.com>
2021-04-14 15:28:08 +0600

    Add files via upload

---
+++ C#/Хребтов_Задание2/Хребтов_Задание2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Хребтов_Задание2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Form2().Show();
+        }
+    }
+}
---
+++ C#/Хребтов_Задание2/Хребтов_Задание2/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Хребтов_Задание2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            GraphicsPath myPath = new GraphicsPath();
+            myPath.AddLines(new[] 
+            { 
+                new Point(0, Height / 2),
+                new Point(Width /2, 0),
+                new Point(Width, Height / 2),
+                new Point(Width / 2, Height)
+            });
+
+            Region = new Region(myPath); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+    }
+}
