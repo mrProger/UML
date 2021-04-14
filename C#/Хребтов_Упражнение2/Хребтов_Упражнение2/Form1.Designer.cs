dc049eaa8a6e7b1f69d87c4bf3d31d5a24097cc8
mrProger <60040129+mrProger@users.noreply.github.com>
2021-04-14 15:28:35 +0600

    Add files via upload

---
+++ C#/Хребтов_Упражнение2/Хребтов_Упражнение2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Хребтов_Упражнение2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExitButton = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.AutoSize = true;
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExitButton.Location = new System.Drawing.Point(122, 115);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(26, 25);
+            this.ExitButton.TabIndex = 1;
+            this.ExitButton.Text = "X";
+            this.ExitButton.Click += new System.EventHandler(this.ExitButtonClick);
+            this.ExitButton.MouseEnter += new System.EventHandler(this.ExitButtonEnter);
+            this.ExitButton.MouseLeave += new System.EventHandler(this.ExitButtonLeave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Brown;
+            this.button1.Location = new System.Drawing.Point(116, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hello!!!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ExitButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.Label ExitButton;
+        private System.Windows.Forms.Button button1;
+
+
+    }
+}
+
---
+++ C#/Хребтов_Упражнение2/Хребтов_Упражнение2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Хребтов_Упражнение2
+{
+    public partial class Form1 : Form
+    {
+        public Form1() { InitializeComponent(); }
+
+        private void ExitButtonClick(object sender, EventArgs e) { Close(); }
+        private void ExitButtonEnter(object sender, EventArgs e) { ExitButton.ForeColor = Color.Blue; }
+        private void ExitButtonLeave(object sender, EventArgs e) { ExitButton.ForeColor = Color.Black; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GraphicsPath myPath = new GraphicsPath(); 
+            //myPath.AddPolygon(new Point[] { new Point(0, 0), new Point(0, this.Height), new Point(this.Width, 0) }); 
+            myPath.AddEllipse(0, 0, this.Width, this.Height);  
+            Region myRegion = new Region(myPath); 
+            this.Region = myRegion;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Form2().Show();
+        }
+    }
+}
